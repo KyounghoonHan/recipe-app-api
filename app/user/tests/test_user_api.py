01317470c6cacbba7259d3ab60252f1089751aaf
khhan <khhan7813@gmail.com>
2021-12-20 16:42:19 +0900

    Added endpoint for creating user

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -7,6 +7,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -49,7 +50,8 @@ class PublicUserApiTests(TestCase):
         """Test that the password must be more than 4 characters"""
         payload = {
             'email': 'test@a.com',
-            'password': '1234'
+            'password': '1234',
+            'name': 'test'
         }
         res = self.client.post(CREATE_USER_URL, payload)
         
@@ -58,3 +60,24 @@ class PublicUserApiTests(TestCase):
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+        
+    def test_create_token_for_user(self):
+        """Test that a token is created for the user"""
+        payload = {
+            'email': 'test@a.com',
+            'password': '12345'
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, payload)
+        
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        
+    def test_create_token_invalid_credentials(self):
+        """Test that token is not created if invalid credentials are given"""
+        create_user(email='test@a.com', password='12345')
+        payload = {'email': 'test@a.com', 'password': '1234'}
+        res = self.client.post(TOKEN_URL, payload)
+        
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
